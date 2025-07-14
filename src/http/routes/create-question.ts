54a0157f2f8f1bb59c95b4a12171dcac94d507ed
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-07-14 19:40:02 -0300

    First Commit

---
+++ src/http/routes/create-question.ts
@@ -0,0 +1,81 @@
+import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
+import { z } from "zod/v4";
+import { schema } from "../../db/schema/index.ts";
+import { db } from "../../db/connection.ts";
+import { generareAnswer, generateEmbeddings } from "../../services/gemini.ts";
+import { and, eq, sql } from "drizzle-orm";
+
+export const CreateQuestionRoute: FastifyPluginCallbackZod = (app) => {
+  app.post(
+    "/rooms/:roomId/questions",
+    {
+      schema: {
+        params: z.object({
+          roomId: z.string(),
+        }),
+        body: z.object({
+          question: z.string().min(1),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { roomId } = request.params;
+      const { question } = request.body;
+
+      const embeddings = await generateEmbeddings(question);
+
+      const embeddingsLiteral = `'[${embeddings.join(",")}]'::vector`;
+
+      const chunks = await db
+        .select({
+          id: schema.audioChunks.id,
+          transcription: schema.audioChunks.transcription,
+          similarity: sql<number>`1 - (${
+            schema.audioChunks.embeddings
+          } <=> ${sql.raw(embeddingsLiteral)})`,
+        })
+        .from(schema.audioChunks)
+        .where(
+          and(
+            eq(schema.audioChunks.roomId, roomId),
+            sql`1 - (${schema.audioChunks.embeddings} <=> ${sql.raw(
+              embeddingsLiteral
+            )}) > 0.7`
+          )
+        )
+        .orderBy(
+          sql`(${schema.audioChunks.embeddings} <=> ${sql.raw(
+            embeddingsLiteral
+          )})`
+        )
+        .limit(3);
+
+      let answer: string | null = null;
+
+      if (chunks.length > 0) {
+        const transcriptions = chunks.map((chunk) => chunk.transcription);
+
+        answer = await generareAnswer(question, transcriptions);
+      }
+
+      const result = await db
+        .insert(schema.questions)
+        .values({
+          roomId,
+          question,
+          answer,
+        })
+        .returning();
+
+      const insertedQuestion = result[0];
+
+      if (!insertedQuestion) {
+        throw new Error("Failed to create new question");
+      }
+
+      return reply
+        .status(201)
+        .send({ questionId: insertedQuestion.id, answer });
+    }
+  );
+};
